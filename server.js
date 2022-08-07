676101cc50df30f25c8140d13b1770f71c87bbb7
Ryan <ryanstevens198@yahoo.com>
2022-08-07 14:08:55 -0700

    adding in heroku for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -19,6 +20,16 @@ mongoose.connect(db)
 
 // Use Routes
 app.use('/api/items', items)
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    // Set the static folder
+    app.use(express.static('client/build'));
+    
+    app.get('*', (reg, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const port = process.env.port || 5000;
 
