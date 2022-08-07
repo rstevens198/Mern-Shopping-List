6158364faa5b07eea925fd05a84a108c995077d8
Ryan <ryanstevens198@yahoo.com>
2022-08-06 22:45:02 -0700

    added in the redux foor the add and delete items.

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store'
+import ItemModal from './components/ItemModal';
+import { Container } from 'reactstrap';
+
 import ShoppingList from './components/ShoppingList';
 
 class App extends Component {
   render(){
     return (
+      <Provider store={store}>
       <div className="App">
         <AppNavbar />
+        <Container>
+        <ItemModal></ItemModal>
         <ShoppingList></ShoppingList>
+        </Container>
       </div>
+      </Provider>
     );
   }
 }
