553d0985196817198385c048dd8e6921ef1f0bd3
Ryan <ryanstevens198@yahoo.com>
2022-08-07 14:42:46 -0700

    Version 1.0.0

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ if(process.env.NODE_ENV === 'production') {
     })
 }
 
-const port = process.env.port || 5000;
+const port = (process.env.port || 5000);
 
 app.listen(port, () => console.log('Server started on port ${port}'));
